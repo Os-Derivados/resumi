2f2cc005ec61c3ecdcb4a1811a90ebeee905e755
trsaints <thiago.dev2k3@gmail.com>
2025-11-30 14:21:57 -0300

    update(backend): configure database connection at appliccation startup

--- backend/Resumi/Program.cs
+++ backend/Resumi/Program.cs
@@ -1,18 +1,31 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Resumi.Infra.Database.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Services
 builder.Services.AddControllers();
+
+// Configure EF Core with Npgsql (PostgreSQL)
+var defaultConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+if (!string.IsNullOrEmpty(defaultConnection))
+{
+    builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(defaultConnection));
+}
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Minha API",
-        Version = "v1",
-        Description = "Descrição breve da API"
-    });
+    c.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "Minha API",
+            Version = "v1",
+            Description = "Descrição breve da API",
+        }
+    );
 });
 
 var app = builder.Build();
