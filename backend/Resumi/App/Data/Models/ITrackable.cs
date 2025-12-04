542a3e80ade037a8b230ff6d60d475fcae7a0817
trsaints <thiago.dev2k3@gmail.com>
2025-12-03 22:53:30 -0300

    docs(ITrackable): include dooumentation regarding contract purpose

--- backend/Resumi/App/Data/Models/ITrackable.cs
+++ backend/Resumi/App/Data/Models/ITrackable.cs
@@ -1,5 +1,8 @@
 namespace Resumi.App.Data.Models;
 
+/// <summary>
+/// Este contrato define propriedades de rastreamento para entidades.
+/// </summary>
 public interface ITrackable
 {
     DateTime CreatedAt { get; set; }
