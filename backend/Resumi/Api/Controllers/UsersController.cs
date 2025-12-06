c41f117be5e6ba828608fdb9548f0bb06e801564
trsaints <thiago.dev2k3@gmail.com>
2025-12-06 19:46:45 -0300

    feat(backend/api): create API controller for exposing AppUser operations

---
+++ backend/Resumi/Api/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Resumi.Api.Data.Models;
+
+namespace Resumi.Api.Controllers;
+
+[ApiController]
+[Route("api/users")]
+[Authorize]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    [AllowAnonymous]
+    public IActionResult Create([FromBody] CreateUserModel model)
+    {
+        throw new NotImplementedException("User creation is not implemented yet.");
+    }
+
+    [HttpGet("/me")]
+    public IActionResult GetAuthor()
+    {
+        throw new NotImplementedException("Retrieving the current user is not implemented yet.");
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult Read(int id)
+    {
+        throw new NotImplementedException("Retrieving a user by ID is not implemented yet.");
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult Update(int id, [FromBody] UpdateUserModel model)
+    {
+        throw new NotImplementedException("Updating a user is not implemented yet.");
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete(int id)
+    {
+        throw new NotImplementedException("Deleting a user is not implemented yet.");
+    }
+}
