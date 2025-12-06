a782abfa968aefc348333921f94056182ade0985
trsaints <thiago.dev2k3@gmail.com>
2025-12-06 20:06:11 -0300

    refactor(backend/app): move generic domain operation contracts to generic interface

--- backend/Resumi/App/Services/Interfaces/IResumeService.cs
+++ backend/Resumi/App/Services/Interfaces/IResumeService.cs
@@ -7,24 +7,8 @@ namespace Resumi.App.Services.Interfaces;
 /// Este contrato fornece APIs de alto nível para operações de domínio relacionadas
 /// a entidades <see cref="Resume"/>.
 /// </summary>
-public interface IResumeService
+public interface IResumeService : IDomainService<Resume>
 {
-	/// <summary>
-	/// Cria um novo currículo.
-	/// </summary>
-	/// <param name="newResume">O currículo a ser criado.</param>
-	/// <returns>O currículo criado.</returns>
-	Task<Result<Resume>> CreateAsync(Resume? newResume);
-
-	/// <summary>
-	/// Busca um currículo pelo seu identificador.
-	/// </summary>
-	/// <param name="id">
-	///	O identificador do currículo a ser buscado.
-	/// </param>
-	/// <returns></returns>
-	Task<Result<Resume>> FindAsync(int id);
-
 	/// <summary>
 	/// Busca currículos pelo identificador do usuário.
 	/// </summary>
@@ -36,27 +20,4 @@ public interface IResumeService
 	/// contendo o resultado da operação.
 	/// </returns>
 	Task<Result<IEnumerable<Resume>>> FindByUserAsync(int userId, int skip = 0, int take = 20);
-
-	/// <summary>
-	/// Atualiza um currículo existente.
-	/// </summary>
-	/// <param name="current">
-	///	O currículo atual a ser atualizado.
-	/// </param>
-	/// <param name="updated">O currículo com as informações atualizadas.</param>
-	/// <returns>
-	///	Uma <see cref="Task"/>, representando uma instância de <see cref="Result{T}"/>,
-	///	contendo o resultado da operação.
-	/// </returns>
-	Task<Result<Resume>> UpdateAsync(Resume? current, Resume? updated);
-
-	/// <summary>
-	/// Exclui um currículo pelo seu identificador.
-	/// </summary>
-	/// <param name="id">O identificador do currículo a ser excluído.</param>
-	/// <returns>
-	///	Uma <see cref="Task"/>, representando uma instância de <see cref="Result{T}"/>,
-	///	contendo o resultado da operação.
-	/// </returns>
-	Task<Result<bool>> DeleteAsync(int id);
 }
