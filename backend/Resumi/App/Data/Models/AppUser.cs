bfed0a520ddaf8a3e7867bd3de9d2170dbbefe38
trsaints <thiago.dev2k3@gmail.com>
2025-12-16 23:47:21 -0300

    update(AppUser): include custom 'FullName' field for holding personal data

--- backend/Resumi/App/Data/Models/AppUser.cs
+++ backend/Resumi/App/Data/Models/AppUser.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace Resumi.App.Data.Models;
 
 public class AppUser : IdentityUser<int>, ITrackable
 {
+    [Required] [StringLength(128)] public string FullName { get; set; }
+
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 
