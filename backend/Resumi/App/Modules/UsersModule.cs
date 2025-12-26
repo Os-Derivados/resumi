b57939cfe4d097a583344ccdf9943094ccec3bff
trsaints <thiago.dev2k3@gmail.com>
2025-12-26 20:16:39 -0300

    update(UsersModule): include entity mapper as shared dependency

--- backend/Resumi/App/Modules/UsersModule.cs
+++ backend/Resumi/App/Modules/UsersModule.cs
@@ -1,20 +1,23 @@
 using Microsoft.AspNetCore.Identity;
 using Resumi.App.Data.Models;
 using Resumi.App.Services.Interfaces;
+using Resumi.Infra.Data.Interfaces;
 using Resumi.Infra.Database.Interfaces;
 
 namespace Resumi.App.Modules;
 
 public class UsersModule : DomainModule<AppUser>
 {
+    public readonly IUserMapper Mapper;
+
     public UsersModule(
         IDomainService<AppUser> service,
         IDomainValidator<AppUser> validator,
         IRepository<AppUser> repository,
         UserManager<AppUser> userManager,
-        RoleManager<IdentityRole<int>> roleManager
-    )
+        RoleManager<IdentityRole<int>> roleManager, IUserMapper mapper)
         : base(service, validator, repository, userManager, roleManager)
     {
+        Mapper = mapper;
     }
 }
