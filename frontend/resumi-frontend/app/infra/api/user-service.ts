c1e768711c668a74c9c15296c024b02ca24ec1dd
trsaints <thiago.dev2k3@gmail.com>
2026-01-02 08:51:53 -0300

    feat(frontend/user-service): implement API integration for '/api/users/login' endpoint

--- frontend/resumi-frontend/app/infra/api/user-service.ts
+++ frontend/resumi-frontend/app/infra/api/user-service.ts
@@ -1,6 +1,10 @@
 import type { CreateUserModel } from "~/data/api/create-user-model";
 import type { ReadUserModel } from "~/data/api/read-user-model";
-import type { Result } from "../result";
+import type { LoginModel } from "~/data/api/login-model";
+import type { AuthResponse } from "~/data/api/auth-response";
+import { BackendUrl } from "./api.constants";
+import { getEnvironmentVariable } from "~/infra/utils/environment-utils";
+import type { Result } from "~/infra/result";
 
 export async function createUserAsync(backendUrl: string, model: CreateUserModel): Promise<Result<ReadUserModel>> {
 	try {
@@ -21,6 +25,33 @@ export async function createUserAsync(backendUrl: string, model: CreateUserModel
 				["unknown", ["An unexpected error occurred while creating the user."]],
 			]),
 			allErrors: ["An unexpected error occurred while creating the user."],
+		}
+	}
+}
+
+export async function loginAsync(model: LoginModel): Promise<Result<AuthResponse>> {
+	try {
+		const backendUrl = getEnvironmentVariable(BackendUrl)
+
+		if (!backendUrl) throw new Error('Backend URL is not defined.')
+
+		const loginResult = await useFetch(`${backendUrl}/users/login`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(model)
+		})
+
+		return loginResult.data.value as Result<AuthResponse>
+	}
+	catch {
+		return {
+			succeeded: false,
+			errors: new Map<string, string[]>([
+				["unknown", ["An unexpected error occurred while logging in."]],
+			]),
+			allErrors: ["An unexpected error occurred while logging in."],
 		}
 	}
 }
