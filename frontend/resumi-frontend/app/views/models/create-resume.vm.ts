46269b199e457771aa8d8be12cf379af34c331f8
filipe <filipefaveroguilardi@gmail.com>
2026-01-12 23:56:37 -0300

    [feat] view model da criacao de curriculos feito

---
+++ frontend/resumi-frontend/app/views/models/create-resume.vm.ts
@@ -0,0 +1,60 @@
+import type { FormSubmitEvent } from "@nuxt/ui";
+import z from "zod";
+import type { CreateResumeModel } from "~/data/api/create-resume-model";
+import { createResumeAsync } from "~/infra/api/resume-service";
+import { isDevelopment } from "~/infra/utils/environment-utils";
+
+export class CreateResumeViewModel {
+    constructor() {
+        this.schema = z.object({
+            title: z.string().min(10).max(255),
+            authtoken: z.string()
+        })
+
+        type CreateResumeSchema = z.infer<typeof this.schema>
+
+        this.state = reactive<Partial<CreateResumeSchema>>({
+            title: "",
+            authtoken: useCookie<string>("auth").value 
+        })
+    }
+
+    public readonly schema
+    public readonly state 
+
+    public requestCreateResume = async (event: FormSubmitEvent<typeof this.state>): Promise<void> => {
+        event.preventDefault();
+
+        const toast = useToast()
+
+        try {
+            const createResumeModel = this.schema.parse(this.state) as CreateResumeModel
+            const result = await createResumeAsync(createResumeModel)
+
+            const resultDisplay = result.succeeded 
+                ? "Login realizado com sucesso!"
+                : "Falha ao realizar o login"
+            
+            toast.add({
+                title: 'Criação de curriculo',
+                description: resultDisplay,
+                color: result.succeeded ? "success" : "error",
+                duration: 5000
+            })
+
+            if (result.succeeded) {
+                await useRouter().push("/resume-editor")
+            }
+        }
+        catch (error) {
+            if (isDevelopment()) console.error(error)
+            
+            toast.add({
+                title: 'Criação de curriculo',
+                description: "Ocorreu um erro ao criar o seu curriculo.",
+                color: "error",
+                duration: 5000
+            })
+        }
+    }
+}
