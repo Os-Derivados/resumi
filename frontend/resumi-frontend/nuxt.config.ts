aee3372d98757a775da483b0336568de79598ceb
trsaints <thiago.dev2k3@gmail.com>
2026-02-16 12:48:10 -0300

    update(frontend): ignore invalid SSL certificates on develompent environment

--- frontend/resumi-frontend/nuxt.config.ts
+++ frontend/resumi-frontend/nuxt.config.ts
@@ -1,31 +1,40 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2025-07-15',
-    devtools: { enabled: true },
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
 
-    modules: [
-        '@nuxt/eslint',
-        '@nuxt/ui',
-        '@nuxt/test-utils',
-        '@nuxt/image',
-        '@nuxt/hints',
-        '@pinia/nuxt'
-    ],
+	modules: [
+		'@nuxt/eslint',
+		'@nuxt/ui',
+		'@nuxt/test-utils',
+		'@nuxt/image',
+		'@nuxt/hints',
+		'@pinia/nuxt'
+	],
 
-    css: ['~/assets/css/main.css'],
-    runtimeConfig: {
-        public: {
-            backendUrl: 'https://localhost:8081/api',
-            environment: 'development'
-        }
-    },
-    icon: {
-        customCollections: [{
-            prefix: "custom",
-            dir: "./app/assets/icons"
-        }]
-    },
-    plugins: [
-        { src: '~/plugins/api.client.ts', mode: 'client' }
-    ],
+	css: ['~/assets/css/main.css'],
+	runtimeConfig: {
+		public: {
+			backendUrl: 'https://localhost:8081/api',
+			environment: 'development'
+		}
+	},
+	icon: {
+		customCollections: [{
+			prefix: "custom",
+			dir: "./app/assets/icons"
+		}]
+	},
+	plugins: [
+		{ src: '~/plugins/api.client.ts', mode: 'client' }
+	],
+
+	// Ignore SSL certificate errors in development
+	hooks: {
+		ready() {
+			if (import.meta.env.NODE_ENV === 'development') {
+				import.meta.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+			}
+		}
+	}
 })
