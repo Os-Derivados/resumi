ca4ec270473402163a2ad4fd1172f3ef53bdeac5
trsaints <thiago.dev2k3@gmail.com>
2026-02-16 13:51:21 -0300

    feat(frontend): create base zod schema for Resume main data

---
+++ frontend/resumi-frontend/app/data/schema/resume.schema.ts
@@ -0,0 +1,17 @@
+import { z, type ZodObject } from "zod";
+
+/**
+ * Define o esquema de validação para um currículo, utilizando a biblioteca Zod.
+ * Este esquema é utilizado para garantir que os dados do currículo estejam no formato correto antes de serem processados ou armazenados.
+ * @returns Um objeto ZodObject que representa o esquema de validação do currículo.
+ */
+export function resumeSchema(): ZodObject {
+	return z.object({
+		title: z.string().min(10).max(255),
+		ownerName: z.string().min(3).max(255),
+		location: z.string().max(255).optional(),
+		email: z.email().max(255),
+		phoneNumber: z.string().max(20),
+		keyword: z.string().max(255).optional()
+	})
+}
