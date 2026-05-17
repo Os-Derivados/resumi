ca9a64b28dbe08f5c3824164576f7c6c1d07c76b
trsaints <thiago.dev2k3@gmail.com>
2026-05-17 19:09:27 -0300

    update(backend): split 'Result' parameters object into two signatures so 'Data' field becomes optional

--- backend/Resumi/Infra/Data/Models/Result.cs
+++ backend/Resumi/Infra/Data/Models/Result.cs
@@ -1,35 +1,100 @@
+using System.Diagnostics.CodeAnalysis;
+using Resumi.Infra.Exceptions;
+
 namespace Resumi.Infra.Data.Models;
 
 /// <summary>
 /// Encapsula o resultado de uma operação, incluindo sucesso ou falha e mensagens associadas.
 /// </summary>
-/// <typeparam name="T">Tipo do valor retornado em caso de sucesso.</typeparam>
-public class Result<T>
+public record Result
 {
-    public bool Succeeded { get; init; }
-    public T? Data { get; init; }
-    public List<string> AllErrors => Errors.Values.SelectMany(e => e).Distinct().ToList();
-    public ResultDictionary Errors { get; init; } = new();
+    protected Result(bool succeeded, ResultDictionary? errors)
+    {
+        if (!succeeded && errors is null)
+        {
+            throw new InfrastructureException("Failed operation must include errors");
+        }
+
+        Succeeded = succeeded;
+        Errors = errors;
+    }
 
     /// <summary>
-    /// Cria um resultado de sucesso contendo os dados fornecidos pela operação realizada.
+    /// Indica se a operação foi bemsucedida.
     /// </summary>
-    /// <param name="data">
-    /// Os dados resultantes da operação bem-sucedida.
-    /// </param>
-    /// <returns>
-    /// Uma instância de <see cref="Result{T}"/> representando o sucesso da operação com os dados fornecidos.
-    /// </returns>
-    public static Result<T> Success(T data) => new() { Succeeded = true, Data = data };
+    [MemberNotNullWhen(false, nameof(Errors))]
+    [MemberNotNullWhen(false, nameof(AllErrors))]
+    public bool Succeeded { get; private init; }
 
-    public static Result<T> Failure(ResultDictionary errors) =>
-        new() { Succeeded = false, Errors = errors };
+    /// <summary>
+    /// Contém a relação chave-valor dos possíveis errors ocorridos na operação.
+    /// </summary>
+    public ResultDictionary? Errors { get; }
 
-    public static Result<T> Failure(string key, string errorMessage)
+    /// <summary>
+    /// Representa a relação de erros <see cref="Errors"/> no formato de lista, sem as chaves de erro.
+    /// </summary>
+    public List<string>? AllErrors => Errors?.Values.SelectMany(e => e).Distinct().ToList();
+
+    /// <summary>
+    /// Indica o sucesso de uma operação.
+    /// </summary>
+    public static Result Success => new(true, null);
+
+    /// <summary>
+    /// Indica o fracasso da operação.
+    /// </summary>
+    /// <param name="result">Uma instância de <see cref="Result"/>, contendo o resultado de uma operação falha.</param>
+    /// <returns>Uma instância de <see cref="Result"/>, indicando o fracasso da operação.</returns>
+    public static Result Failed(Result result) => new(succeeded: false, result.Errors);
+
+    /// <summary>
+    /// Indica o fracasso da operação.
+    /// </summary>
+    /// <param name="errors">Uma instância de <see cref="ResultDictionary"/>, contendo a relação de erros da operação.</param>
+    /// <returns>Uma instância de <see cref="Result"/>, indicando o fracasso da operação.</returns>
+    public static Result Failed(ResultDictionary errors) => new(false, errors);
+}
+
+/// <summary>
+/// Encapsula o resultado de uma operação, incluindo sucesso ou falha e mensagens associadas.
+/// </summary>
+/// <typeparam name="T">Tipo do valor retornado em caso de sucesso.</typeparam>
+public record Result<T> : Result
+{
+    protected Result(bool succeeded, ResultDictionary? errors, T? data) : base(succeeded, errors)
     {
-        ResultDictionary errors = [];
-        errors.AddError(key, errorMessage);
+        if (succeeded && data is null)
+        {
+            throw new InfrastructureException("Succeeded operation must include data");
+        }
 
-        return Failure(errors);
+        Data = data;
     }
+
+    /// <summary>
+    /// Representa a informação resultante da operação.
+    /// </summary>
+    public T? Data { get; private init; }
+
+    /// <summary>
+    /// Indica o successo da operação.
+    /// </summary>
+    /// <param name="data">O valor retornado pela operação.</param>
+    /// <returns>Uma instância de <see cref="Result"/>, indicando o sucesso da operação.</returns>
+    public new static Result<T> Success(T data) => new(succeeded: true, errors: null, data);
+
+    /// <summary>
+    /// Indica o fracasso da operação.
+    /// </summary>
+    /// <param name="result">Uma instância de <see cref="Result"/>, contendo o resultado de uma operação falha.</param>
+    /// <returns>Uma instância de <see cref="Result"/>, indicando o fracasso da operação.</returns>
+    public new static Result<T> Failed(Result result) => new(succeeded: false, result.Errors, data: default);
+
+    /// <summary>
+    /// Indica o fracasso da operação.
+    /// </summary>
+    /// <param name="errors">Uma instância de <see cref="ResultDictionary"/>, contendo a relação de erros da operação.</param>
+    /// <returns>Uma instância de <see cref="Result"/>, indicando o fracasso da operação.</returns>
+    public new static Result<T> Failed(ResultDictionary errors) => new(false, errors, data: default);
 }
