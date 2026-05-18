46f3e7a62e67f067f8d1f05736c5a55aa89a3c35
trsaints <thiago.dev2k3@gmail.com>
2026-05-18 00:05:16 -0300

    update(backend): include optional properties on 'UpdateResumeModel'

--- backend/Resumi/Api/Data/Models/UpdateResumeModel.cs
+++ backend/Resumi/Api/Data/Models/UpdateResumeModel.cs
@@ -1,10 +1,17 @@
 using System.ComponentModel.DataAnnotations;
+using Resumi.Domain.Models;
 
 namespace Resumi.Api.Data.Models;
 
 /// <summary>
 /// Representa um objeto de parâmetros para atualizar um <see cref="Resume"/> existente.
 /// </summary>
-/// <param name="Title">O possível novo título do currículo.</param>
 /// <param name="UserId">O identificador do usuário proprietário do currículo.</param>
-public record UpdateResumeModel(string? Title, [Required] int UserId);
+/// <param name="Title">O possível novo título do currículo.</param>
+public record UpdateResumeModel(
+    [Required] int UserId,
+    [MaxLength(128)] string? Title,
+    [MaxLength(128)] string? OwnerName,
+    string? Location,
+    [EmailAddress] string? Email,
+    [Phone] string? PhoneNumber);
