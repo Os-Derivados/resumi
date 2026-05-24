2b8c2a8e4c3588e46a175b90b45a2a9045598e8b
trsaints <thiago.dev2k3@gmail.com>
2026-05-23 21:21:08 -0300

    update(backend): override 'Entity.ShallowCopy' on 'Degree' domain model

--- backend/Resumi/Domain/Models/Degree.cs
+++ backend/Resumi/Domain/Models/Degree.cs
@@ -9,7 +9,44 @@ namespace Resumi.Domain.Models;
 [Table("Degrees")]
 public class Degree : ResumeNode
 {
-    public string? Highlights { get; set; }
+	public static readonly string FailedToCreate = "Não foi possível cadastrar formação acadêmica.";
 
-    [Required] public DegreeLevel Level { get; set; }
+	public string? Highlights { get; set; }
+
+	[Required] public DegreeLevel Level { get; set; }
+
+	public override Degree ShallowCopy()
+	{
+		return new Degree
+		{
+			#region Campos de Entity
+
+			Id = Id,
+			CreatedAt = CreatedAt,
+			UpdatedAt = UpdatedAt,
+
+			#endregion
+
+			#region Campos de ResumeNode
+
+			ResumeId = ResumeId,
+			Name = Name,
+			Description = Description,
+			InstitutionName = InstitutionName,
+			Location = Location,
+			IsRemote = IsRemote,
+			StartDate = StartDate,
+			EndDate = EndDate,
+			StillEngaged = StillEngaged,
+
+			#endregion
+
+			#region Campos de Degree
+
+			Highlights = Highlights,
+			Level = Level
+
+			#endregion
+		};
+	}
 }
