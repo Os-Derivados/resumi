ceb86936fe7687eafbea82b55418de4d5c8699e6
trsaints <thiago.dev2k3@gmail.com>
2026-05-23 23:20:46 -0300

    feat(backend): define readonly domain-API translation for 'Volunteership' entities as projection

---
+++ backend/Resumi/Infra/Projections/VolunteershipProjections.cs
@@ -0,0 +1,24 @@
+using System.Linq.Expressions;
+using Resumi.Api.Data.Models;
+using Resumi.Domain.Models;
+
+namespace Resumi.Infra.Projections;
+
+using VolunteershipProjection = Expression<Func<Volunteership, VolunteershipModel>>;
+
+public static class VolunteershipProjections
+{
+	public static readonly VolunteershipProjection Basic = v => new VolunteershipModel
+	{
+		Id = v.Id,
+		ResumeId = v.ResumeId,
+		Name = v.Name,
+		Description = v.Description,
+		InstitutionName = v.InstitutionName,
+		Location = v.Location,
+		IsRemote = v.IsRemote,
+		StartDate = v.StartDate,
+		EndDate = v.EndDate,
+		StillEngaged = v.StillEngaged
+	};
+}
