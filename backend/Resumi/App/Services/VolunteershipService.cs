0b89f38318b406b2b18d369058c7588a26b6b703
trsaints <thiago.dev2k3@gmail.com>
2026-05-23 21:13:53 -0300

    update(backend): adjust 'VolunteershipService' signatures to handle dtos instead of domain models

--- backend/Resumi/App/Services/VolunteershipService.cs
+++ backend/Resumi/App/Services/VolunteershipService.cs
@@ -1,30 +1,32 @@
+using Resumi.Api.Data.Models;
+using Resumi.Domain.Models;
 using Resumi.Infra.Data.Models;
 
 namespace Resumi.App.Services;
 
-public class VolunteershipService : IVolunteershipService
+public class VolunteershipService
 {
-    public Task<Result<Volunteership>> CreateAsync(Volunteership? newEntity)
+    public Task<Result<VolunteershipModel>> CreateAsync(Volunteership? newEntity)
     {
         throw new NotImplementedException();
     }
 
-    public Task<Result<Volunteership>> FindAsync(int id)
+    public Task<Result<VolunteershipModel>> FindAsync(int id)
     {
         throw new NotImplementedException();
     }
 
-    public Task<Result<IEnumerable<Volunteership>>> FindAllAsync(int skip = 0, int take = 20)
+    public Task<Result<List<VolunteershipModel>>> FindAllAsync(int resumeId, int skip = 0, int take = 20)
     {
         throw new NotImplementedException();
     }
 
-    public Task<Result<Volunteership>> UpdateAsync(Volunteership? current, Volunteership? updated)
+    public Task<Result<VolunteershipModel>> UpdateAsync(Volunteership? current, Volunteership? updated)
     {
         throw new NotImplementedException();
     }
 
-    public Task<Result<bool>> DeleteAsync(int id)
+    public Task<Result> DeleteAsync(int id)
     {
         throw new NotImplementedException();
     }
