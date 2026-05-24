4ee5e75e97c81746f1b58472655572a94374c01c
trsaints <thiago.dev2k3@gmail.com>
2026-05-23 23:16:42 -0300

    feat(backend): implement domain-level 'Experience' operations

--- backend/Resumi/App/Services/ExperienceService.cs
+++ backend/Resumi/App/Services/ExperienceService.cs
@@ -1,31 +1,110 @@
+using Microsoft.EntityFrameworkCore;
+using Resumi.Api.Data.Models;
+using Resumi.Domain.Models;
+using Resumi.Domain.Validators.Interfaces;
 using Resumi.Infra.Data.Models;
+using Resumi.Infra.Database.Context;
+using Resumi.Infra.Projections;
 
 namespace Resumi.App.Services;
 
-public class ExperienceService : IExperienceService
+public class ExperienceService(
+    AppDbContext dbContext,
+    ILogger<ExperienceService> logger,
+    IDomainValidator<Experience> validator)
 {
-    public Task<Result<Experience>> CreateAsync(Experience? newEntity)
+    public async Task<Result<ExperienceModel>> CreateAsync(Experience? newEntity)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var validationResult = validator.ValidateCreation(newEntity);
+
+            if (!validationResult.Succeeded)
+            {
+                return Result<ExperienceModel>.Failure(validationResult);
+            }
+
+            var creationResult = await dbContext.Experiences.AddAsync(newEntity!);
+            var createdEntries = await dbContext.SaveChangesAsync();
+
+            if (creationResult.State is not EntityState.Added || createdEntries == 0)
+            {
+                return Result<ExperienceModel>.Failure(nameof(Experience), Experience.FailedToCreate);
+            }
+
+            var createdExperience = await dbContext.Experiences.AsNoTracking()
+                .Select(ExperienceProjections.Basic).FirstOrDefaultAsync(e => e.Id == newEntity!.Id);
+
+            return Result<ExperienceModel>.Success(createdExperience!);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to create experience: {Message}", ex.Message);
+
+            return Result<ExperienceModel>.Failure(nameof(Experience), Experience.InternalError);
+        }
     }
 
-    public Task<Result<Experience>> FindAsync(int id)
+    public async Task<Result<ExperienceModel>> UpdateAsync(Experience? current, Experience? updated)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var validationResult = validator.ValidateUpdate(current, updated);
+
+            if (!validationResult.Succeeded)
+            {
+                return Result<ExperienceModel>.Failure(validationResult);
+            }
+
+            var updateResult = dbContext.Experiences.Update(updated!);
+            var updatedEntries = await dbContext.SaveChangesAsync();
+
+            if (updateResult.State is not EntityState.Modified || updatedEntries is 0)
+            {
+                return Result<ExperienceModel>.Failure(nameof(Experience), Experience.FailedToUpdate);
+            }
+
+            var updatedExperience = await dbContext.Experiences
+                .AsNoTracking()
+                .Select(ExperienceProjections.Basic)
+                .FirstOrDefaultAsync(e => e.Id == updated!.Id);
+
+            return Result<ExperienceModel>.Success(updatedExperience!);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to update experience: {Message}", ex.Message);
+
+            return Result<ExperienceModel>.Failure(nameof(Experience), Experience.InternalError);
+        }
     }
 
-    public Task<Result<IEnumerable<Experience>>> FindAllAsync(int skip = 0, int take = 20)
+    public async Task<Result> DeleteAsync(int id)
     {
-        throw new NotImplementedException();
-    }
+        try
+        {
+            var target = await dbContext.Experiences.AsNoTracking().FirstOrDefaultAsync(e => e.Id == id);
 
-    public Task<Result<Experience>> UpdateAsync(Experience? current, Experience? updated)
-    {
-        throw new NotImplementedException();
-    }
+            if (target is null)
+            {
+                return Result.Failure(nameof(Experience), Experience.NotFound);
+            }
 
-    public Task<Result<bool>> DeleteAsync(int id)
-    {
-        throw new NotImplementedException();
+            var removalResult = dbContext.Experiences.Remove(target);
+            var removedEntries = await dbContext.SaveChangesAsync();
+
+            if (removalResult.State is not EntityState.Deleted || removedEntries is 0)
+            {
+                return Result.Failure(nameof(Experience), Experience.FailedToDelete);
+            }
+
+            return Result.Success;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to delete experience: {Message}", ex.Message);
+
+            return Result.Failure(nameof(Experience), Experience.InternalError);
+        }
     }
 }
