b2e7f32fd1a0de12950d772622b6e88d6b784495
trsaints <thiago.dev2k3@gmail.com>
2026-05-23 22:34:26 -0300

    feat(backend): define readonly domain-API translation for 'Experience' entities as projection

---
+++ backend/Resumi/Infra/Projections/ExperienceProjections.cs
@@ -0,0 +1,24 @@
+using System.Linq.Expressions;
+using Resumi.Api.Data.Models;
+using Resumi.Domain.Models;
+
+namespace Resumi.Infra.Projections;
+
+using ExperienceProjection = Expression<Func<Experience, ExperienceModel>>;
+
+public static class ExperienceProjections
+{
+	public static readonly ExperienceProjection Basic = e => new ExperienceModel
+	{
+		Id = e.Id,
+		ResumeId = e.ResumeId,
+		Name = e.Name,
+		Description = e.Description,
+		InstitutionName = e.InstitutionName,
+		Location = e.Location,
+		IsRemote = e.IsRemote,
+		StartDate = e.StartDate,
+		EndDate = e.EndDate,
+		StillEngaged = e.StillEngaged,
+	};
+}
