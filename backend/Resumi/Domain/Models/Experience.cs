aad162b91c2cdfd26a90a5cc5e7fb03cae58696e
trsaints <thiago.dev2k3@gmail.com>
2026-05-23 23:01:18 -0300

    fix(backend): ensure 'Experience' implements 'Entity.ShallowCopy'

--- backend/Resumi/Domain/Models/Experience.cs
+++ backend/Resumi/Domain/Models/Experience.cs
@@ -5,8 +5,42 @@ namespace Resumi.Domain.Models;
 /// <summary>
 /// Representa uma experiência profissional dentro de um <see cref="Resume"/>.
 /// </summary>
-[Table(("Experiences"))]
+[Table("Experiences")]
 public class Experience : ResumeNode
 {
-    public string? Highlights { get; set; }
+	public string? Highlights { get; set; }
+
+	public override Experience ShallowCopy()
+	{
+		return new Experience
+		{
+			#region Campos de Entity
+
+			Id = Id,
+			CreatedAt = CreatedAt,
+			UpdatedAt = UpdatedAt,
+
+			#endregion
+
+			#region Campos de ResumeNode
+
+			ResumeId = ResumeId,
+			Name = Name,
+			Description = Description,
+			InstitutionName = InstitutionName,
+			Location = Location,
+			IsRemote = IsRemote,
+			StartDate = StartDate,
+			EndDate = EndDate,
+			StillEngaged = StillEngaged,
+
+			#endregion
+
+			#region Campos de Degree
+
+			Highlights = Highlights,
+
+			#endregion
+		};
+	}
 }
