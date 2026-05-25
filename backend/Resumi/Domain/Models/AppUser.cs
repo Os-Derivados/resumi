5812a552d2d9d922eb891f5257869688a8a55c45
trsaints <thiago.dev2k3@gmail.com>
2026-05-24 22:28:34 -0300

    feat(backend): implement 'ICloneableEntity<AppUser>'

--- backend/Resumi/Domain/Models/AppUser.cs
+++ backend/Resumi/Domain/Models/AppUser.cs
@@ -3,19 +3,44 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Resumi.Domain.Models;
 
-public class AppUser : IdentityUser<int>, ITrackable
+public class AppUser : IdentityUser<int>, ITrackable, ICloneableEntity<AppUser>
 {
-    public static readonly string NotFound = "Usuário não encontrado.";
-    public static readonly string FailedToCreate = "Falha ao criar usuário.";
-    public static readonly string FailedToUpdate = "Falha ao atualizar usuário.";
-    public static readonly string FailedToDelete = "Falha ao excluir usuário.";
-    public static readonly string InvalidState = "O Usuário encontra-se em um estado inválido para esta operação.";
-    public static readonly string InternalError = "Ocorreu um erro interno ao processar a solicitação.";
+	public static readonly string NotFound = "Usuário não encontrado.";
+	public static readonly string FailedToCreate = "Falha ao criar usuário.";
+	public static readonly string FailedToUpdate = "Falha ao atualizar usuário.";
+	public static readonly string FailedToDelete = "Falha ao excluir usuário.";
+	public static readonly string InvalidState = "O Usuário encontra-se em um estado inválido para esta operação.";
+	public static readonly string InternalError = "Ocorreu um erro interno ao processar a solicitação.";
 
-    [Required] [StringLength(128)] public required string FullName { get; set; }
+	[Required] [StringLength(128)] public required string FullName { get; set; }
 
-    public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
+	public DateTime CreatedAt { get; set; }
+	public DateTime? UpdatedAt { get; set; }
 
-    public ICollection<Resume> Resumes { get; set; } = [];
+	public ICollection<Resume> Resumes { get; set; } = [];
+
+	public AppUser ShallowCopy()
+	{
+		return new AppUser
+		{
+			Id = Id,
+			UserName = UserName,
+			NormalizedUserName = NormalizedUserName,
+			Email = Email,
+			NormalizedEmail = NormalizedEmail,
+			EmailConfirmed = EmailConfirmed,
+			PasswordHash = PasswordHash,
+			SecurityStamp = SecurityStamp,
+			ConcurrencyStamp = ConcurrencyStamp,
+			PhoneNumber = PhoneNumber,
+			PhoneNumberConfirmed = PhoneNumberConfirmed,
+			TwoFactorEnabled = TwoFactorEnabled,
+			LockoutEnd = LockoutEnd,
+			LockoutEnabled = LockoutEnabled,
+			AccessFailedCount = AccessFailedCount,
+			FullName = FullName,
+			CreatedAt = CreatedAt,
+			UpdatedAt = UpdatedAt
+		};
+	}
 }
