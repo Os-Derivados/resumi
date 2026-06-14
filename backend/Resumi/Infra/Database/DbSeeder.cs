55ad7ef260a9c99897d7e9f9a1e89c210314f86e
trsaints <thiago.dev2k3@gmail.com>
2026-06-14 12:46:45 -0300

    fix(backend): ensure seeding occurs during transaction

--- backend/Resumi/Infra/Database/DbSeeder.cs
+++ backend/Resumi/Infra/Database/DbSeeder.cs
@@ -1,7 +1,10 @@
 using System.Text.Json;
 using Microsoft.AspNetCore.Identity;
 using Resumi.Api.Data.Models;
+using Resumi.Domain.Models;
 using Resumi.Infra.Constants;
+using Resumi.Infra.Data.Mappers;
+using Resumi.Infra.Database.Context;
 using Resumi.Infra.Exceptions;
 
 namespace Resumi.Infra.Database;
@@ -10,10 +13,13 @@ public static class DbSeeder
 {
     public static async Task<bool> SeedDatabaseAsync(IServiceProvider provider)
     {
+        var dbContext = provider.GetRequiredService<AppDbContext>()
+                        ?? throw new InfrastructureException("AppDbContext service is not available.");
+
         var userManager = provider.GetRequiredService<UserManager<AppUser>>()
                           ?? throw new InfrastructureException("UserManager service is not available.");
 
-        var userMapper = provider.GetRequiredService<IUserMapper>()
+        var userMapper = provider.GetRequiredService<UserMapper>()
                          ?? throw new InfrastructureException("UserMapper service is not available.");
 
         var adminUserJson = Environment.GetEnvironmentVariable(EnvironmentVariables.AdminUser);
@@ -21,14 +27,33 @@ public static class DbSeeder
                              ?? throw new InfrastructureException(
                                  "Failed to deserialize admin user data from environment variable.");
 
-        var existingAdmin = await userManager.FindByEmailAsync(adminUserModel.Email);
+        await using var transaction = await dbContext.Database.BeginTransactionAsync();
 
-        if (existingAdmin is not null) return false;
+        try
+        {
+            var existingAdmin = await userManager.FindByEmailAsync(adminUserModel.Email);
 
-        var newAdminUser = userMapper.NewDomainModel(adminUserModel)
-                           ?? throw new InfrastructureException("Failed to map admin user model to domain model.");
-        var createResult = await userManager.CreateAsync(newAdminUser, adminUserModel.Password);
+            if (existingAdmin is not null) return false;
 
-        return createResult.Succeeded;
+            var newAdminUser = userMapper.NewDomainModel(adminUserModel) ??
+                               throw new InfrastructureException("Failed to map admin user model to domain model.");
+            var createResult = await userManager.CreateAsync(newAdminUser, adminUserModel.Password);
+
+            if (!createResult.Succeeded) return false;
+
+            var addToRoleResult = await userManager.AddToRoleAsync(newAdminUser, AuthConstants.AdminRole);
+
+            if (!addToRoleResult.Succeeded) return false;
+
+            await transaction.CommitAsync();
+
+            return true;
+        }
+        catch
+        {
+            await transaction.RollbackAsync();
+
+            throw;
+        }
     }
 }
