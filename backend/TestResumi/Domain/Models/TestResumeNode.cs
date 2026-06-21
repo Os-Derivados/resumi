7bc46c93f47ee6e764cd5f4f896fa19181000931
trsaints <thiago.dev2k3@gmail.com>
2026-06-20 21:49:45 -0300

    test(backend): ensure 'ResumeNode' throws exception with invalid 'StilEngaged' state

---
+++ backend/TestResumi/Domain/Models/TestResumeNode.cs
@@ -0,0 +1,26 @@
+using Resumi.Domain.Exceptions;
+using Resumi.Domain.Models;
+
+namespace TestResumi.Domain.Models;
+
+public class TestResumeNode
+{
+    [Fact]
+    public void StillEngaged_WhenSetToFalse_ThrowsException_IfEndDateIsNull()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Throws<StillEngagedException>(() => new Volunteership
+            {
+                ResumeId = 1,
+                Name = "Test Node",
+                Description = "Test Description",
+                InstitutionName = "Test Institution",
+                StartDate = DateTime.Now.AddYears(-1),
+                EndDate = DateTime.Now,
+                StillEngaged = true,
+            }
+        );
+    }
+}
