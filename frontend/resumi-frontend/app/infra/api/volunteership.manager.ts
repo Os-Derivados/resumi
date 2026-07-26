77d3ca04d76c1816d278628e300da100d0c35cdc
trsaints <thiago.dev2k3@gmail.com>
2026-07-26 00:07:30 -0300

    feat(frontend): mapping existing API endpoints for 'Volunteership' entities

--- frontend/resumi-frontend/app/infra/api/volunteership.manager.ts
+++ frontend/resumi-frontend/app/infra/api/volunteership.manager.ts
@@ -1,3 +1,32 @@
+import type { VolunteershipModel } from "~/data/api/models/volunteership.model";
+import type { Result, ValueResult } from "../result";
+import type { AddVolunteershipRequest } from "~/data/api/requests/add-volunteership.request";
+import type { UpdateVolunteershipRequest } from "~/data/api/requests/update-volunteership.request";
+
 const route = '/volunteerships'
 
-export async function addVolunteershipAsync(model: )
+export async function addVolunteershipAsync(model: AddVolunteershipRequest): Promise<ValueResult<VolunteershipModel>> {
+	const { $clientApi } = useNuxtApp()
+
+	return await $clientApi<ValueResult<VolunteershipModel>>(route, {
+		method: 'POST',
+		body: model
+	})
+}
+
+export async function updateVolunteershipAsync(model: UpdateVolunteershipRequest): Promise<ValueResult<VolunteershipModel>> {
+	const { $clientApi } = useNuxtApp()
+
+	return await $clientApi<ValueResult<VolunteershipModel>>(`${route}/${model.id}`, {
+		method: 'PUT',
+		body: model
+	})
+}
+
+export async function deleteVolunteershipAsync(id: number): Promise<Result> {
+	const { $clientApi } = useNuxtApp()
+
+	return await $clientApi<Result>(`${route}/${id}`, {
+		method: 'DELETE'
+	})
+}
